d4ab086c3546c29baa3f2e8d27314aac1005dfa5
Someone68 <68.someone.m@gmail.com>
2023-11-11 14:21:46 -0800

    remove dead code

--- jsexe.js
+++ jsexe.js
@@ -1,8 +1,6 @@
 /// script_execution.js
-let focusedclipboardwritetext = false;
 window.addEventListener("keyup", (event) => {
 	if (event.ctrlKey && event.altKey && event.key === "]") {
-		focusedclipboardwritetext = false;
 		let command = prompt("Evaluate Command:");
 		try {
 			let evaledCommand = eval(command);

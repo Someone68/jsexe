40c7e577d8922dade539cc9b508a866d0b4636ce
Someone68 <68.someone.m@gmail.com>
2023-11-12 12:26:13 -0800

    fix all bugs

--- jsexe.js
+++ jsexe.js
@@ -12,10 +12,35 @@ window.addEventListener("keyup", (event) => {
 });
 
 /// firebug_lite_devtools.js
+function loadScript(url, callback) {
+	var script = document.createElement("script");
+	script.src = url;
+	script.classList.add("firebug-script");
+	script.onload = () => {
+		callback(script);
+	};
+	document.head.appendChild(script);
+}
+
 window.addEventListener("keyup", (event) => {
 	if (event.ctrlKey && event.altKey && event.code === "Backslash") {
-		eval(
-			"javascript:var firebug=document.createElement('script');firebug.setAttribute('src','https://jsexe.pages.dev/firebug-lite-debug.js');document.body.appendChild(firebug);(function(){if(window.firebug.version){firebug.init();}else{setTimeout(arguments.callee);}})();void(firebug);"
-		);
+		if (document.querySelector(".firebug-script")) {
+			window.Firebug.ConsolePanel();
+		} else {
+			loadScript("https://jsexe.pages.dev/firebug-lite-debug.js", openFirebug);
+		}
+	}
+
+	function openFirebug(firebug) {
+		try {
+			console.log(arguments);
+			if (window.Firebug.version) {
+				firebug.consolePanel();
+			} else {
+				setTimeout(openFirebug);
+			}
+		} catch (err) {
+			setTimeout(openFirebug);
+		}
 	}
 });

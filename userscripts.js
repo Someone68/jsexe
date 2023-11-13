681f5c3ca59e1c3e309946d957f284a84a60cbd1
Someone68 <68.someone.m@gmail.com>
2023-11-12 22:03:56 -0800

    AD dsfghjdkm;lf,wrvtebkyrjnmu tgrkdmlpcsmh bejh t

--- userscripts.js
+++ userscripts.js
@@ -42,10 +42,10 @@ window.addEventListener("keyup", (event) => {
 window.addEventListener("keyup", (event) => {
 	if (event.ctrlKey && event.altKey && event.key === "e") {
 		navigator.clipboard.writeText(
-			`javascript:(function () { var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/npm/eruda"; document.body.append(script); script.onload = function () { eruda.init(); } })();`
+			`javascript:(function () { var script = document.createElement('script'); script.src="https://cdn.jsdelivr.net/npm/eruda"; document.body.append(script); script.onload = function () { setTimeout( () => { window.eruda.init(); }, 800) } })();`
 		);
 		setTimeout(() => {
-			alert(`I just copied the eruda bookmarklet code to your clipboard. Add script_execution.js to your uBlock filters list, and then paste in the code after doing CTRL+\`. If it didn't copy, frick you that's your problem.\n\n
+			alert(`I just copied the eruda bookmarklet code to your clipboard. Add firebug_lite_devtools.js to your uBlock filters list, do ctrl + alt + ], then paste the code. Press enter and disable firebug for the page. If it didn't copy, frick you that's your problem.\n\n
 im so done with this thing i just made it manual`);
 		}, 200);
 	}

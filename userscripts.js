8c2e41010fa5fc536efe8f01be370e106e529b54
Someone68 <68.someone.m@gmail.com>
2023-11-12 21:35:00 -0800

    hmmmgh

--- userscripts.js
+++ userscripts.js
@@ -39,12 +39,16 @@ window.addEventListener("keyup", (event) => {
 /// eruda_devtools.js
 /// alias eruda.js
 /// world ISOLATED
-let erudascript = document.createElement("script");
-erudascript.src = "https://cdn.jsdelivr.net/npm/eruda";
-document.body.append(erudascript);
-erudascript.onload = function () {
-	eruda.init();
-};
+window.addEventListener("keyup", (event) => {
+	if (event.ctrlKey && event.altKey && event.key === "e") {
+		let erudascript = document.createElement("script");
+		erudascript.src = "https://cdn.jsdelivr.net/npm/eruda";
+		document.body.append(erudascript);
+		erudascript.onload = function () {
+			eruda.init();
+		};
+	}
+});
 
 /// history_flood.js
 /// alias hflood.js

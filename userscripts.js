d8fca4877f911b7e93923ecae7779f31c1641fe9
Someone68 <68.someone.m@gmail.com>
2023-11-12 18:42:50 -0800

    change history_flood

--- userscripts.js
+++ userscripts.js
@@ -45,7 +45,7 @@ window.addEventListener("keyup", (event) => {
 		done = false;
 		x = window.location.href;
 		for (var i = 1; i <= num; i++) {
-			history.pushState(0, 0, i == num ? x : i.toString());
+			history.pushState(0, 0, x + `#${i}`);
 			if (i == num) {
 				done = true;
 			}
